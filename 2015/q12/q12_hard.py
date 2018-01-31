70c23a44c69bdb35e818b0a0481ea94639e2898d
Lucas Meireles <meireles31@msn.com>
2018-01-31 14:55:15 -0200

    Added solution to 2015 Q12

---
+++ 2015/q12/q12_hard.py
@@ -0,0 +1,30 @@
+import json
+
+def json_sum(data):
+    data_type = type(data)
+    if data_type == int:
+        return data
+    if data_type == list:
+        return sum([json_sum(x) for x in data])
+    if data_type != dict:
+        return 0
+    cur_sum = 0
+    dict_props = data.values()
+    if 'red' in dict_props:
+        return 0
+    for key, val in data.items():
+        cur_sum += json_sum(key)
+        cur_sum += json_sum(val)
+    return cur_sum
+
+def get_json_sum(entry):
+    data = json.loads(entry)
+    return json_sum(data)
+
+def main():
+    entry = input()
+    resp = get_json_sum(entry)
+    print(resp)
+
+if __name__ == '__main__':
+    main()
