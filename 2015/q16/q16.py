38241087f61c355855c127df85692544359603e4
Lucas Meireles <meireles31@msn.com>
2018-01-31 20:14:20 -0200

    Added solution to 2015 Q16

---
+++ 2015/q16/q16.py
@@ -0,0 +1,52 @@
+def parse(entry_list):
+    v = []
+    for entry in entry_list:
+        sue_obj = {}
+        props = ''.join(entry.split(':')[1:]).split(',')
+        for prop_str in props:
+            name, val = prop_str.split()
+            sue_obj[name] = int(val)
+        v.append(sue_obj)
+    return v
+
+def process_entry(entry_list):
+    sue_list = parse(entry_list)
+    target = {
+        'children': 3,
+        'cats': 7,
+        'samoyeds': 2,
+        'pomeranians': 3,
+        'akitas': 0,
+        'vizslas': 0,
+        'goldfish': 5,
+        'trees': 3,
+        'cars': 2,
+        'perfumes': 1
+    }
+    for i, sue in enumerate(sue_list):
+        is_right = True
+        for target_prop, target_val in target.items():
+            if target_prop in sue and sue[target_prop] != target_val:
+                is_right = False
+                break
+        for prop, val in sue.items():
+            if val != target[prop]:
+                is_right = False
+                break
+
+        if is_right:
+            return i+1, sue
+    return None
+
+def main():
+    entry_list = []
+    while True:
+        entry = input()
+        if entry == '':
+            break
+        entry_list.append(entry)
+    resp = process_entry(entry_list)
+    print(resp)
+
+if __name__ == '__main__':
+    main()
