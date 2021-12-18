3ad6b2d799719139234095e067c264941f5e5551
Lucas Meireles <lmeireles@palantir.com>
2021-12-18 11:19:46 -0300

    Added solution to 2021 Q16 part 2

---
+++ 2021/q16/q16_hard.nim
@@ -0,0 +1,123 @@
+import sequtils
+import strutils
+import sugar
+import sets
+import tables
+import bitops
+
+type ParsePacketResponse = object
+    nextIndex: int
+    cumulativeValue: int
+    shouldStop: bool
+
+proc getInput(): string =
+    return readAll(stdin).strip()
+
+proc toBits(n: int): seq[int] =
+    var resp: seq[int] = @[bitand(n, 8), bitand(n, 4), bitand(n, 2), bitand(n, 1)].map(x => (if x != 0: 1 else: 0))
+    return resp
+
+proc toBits(c: char): seq[int] =
+    return toBits(parseHexInt($c))
+
+proc toBits(s: string): seq[int] =
+    var resp: seq[int] = @[]
+    for c in s:
+        resp = resp & toBits(c)
+    return resp
+
+proc bitToInt(bits: seq[int]): int =
+    var resp = 0
+    for i in 0..bits.high:
+        resp += bits[i] * (1 shl (bits.high() - i))
+    return resp
+
+proc processValues(l: seq[int], typeId: int): int =
+    case typeId:
+        of 0:
+            return l.foldl(a+b)
+        of 1:
+            return l.foldl(a*b, 1)
+        of 2:
+            return l.foldl(min(a,b))
+        of 3:
+            return l.foldl(max(a,b))
+        of 5:
+            return l.foldl((if a > b: 1 else : 0))
+        of 6:
+            return l.foldl((if a < b: 1 else : 0))
+        of 7:
+            return l.foldl((if a == b: 1 else : 0))
+        else:
+            return -10000
+
+proc processPacket(input: seq[int], startIndex: int): ParsePacketResponse =
+    let version = bitToInt(input[startIndex..startIndex+2])
+    let typeId = bitToInt(input[startIndex+3..startIndex+5])
+    let payloadStart = startIndex+6
+
+    var i = payloadStart
+
+    if typeId == 4:
+        var literal: seq[int] = @[]
+        while true:
+            let isLast = input[i] == 0
+            for j in 1..4:
+                inc i
+                if i < input.len():
+                    literal.add(input[i])
+                else:
+                    literal.add(0)
+            if isLast:
+                break
+            inc i
+
+        return ParsePacketResponse(nextIndex: i+1, cumulativeValue: bitToInt(literal), shouldStop: true)
+
+    let lenghTypeId = input[payloadStart]
+    var packetVals: seq[int] = @[]
+
+    if lenghTypeId == 0:
+        let bitsToRead = 15
+        let packetLen = bitToInt(input[payloadStart + 1.. payloadStart + bitsToRead])
+        let originalPStart = payloadStart + bitsToRead + 1
+        var pStart = originalPStart
+
+        while pStart - originalPStart < packetLen:
+            let next = processPacket(input, pStart)
+            packetVals.add(next.cumulativeValue)
+            pStart = next.nextIndex
+
+        let response = processValues(packetVals, typeId)
+        return ParsePacketResponse(nextIndex: pStart, cumulativeValue: response, shouldStop:true)
+
+    elif lenghTypeId == 1:
+        let bitsToRead = 11
+        let packetLen = bitToInt(input[payloadStart + 1.. payloadStart + bitsToRead])
+        var pStart = payloadStart + bitsToRead + 1
+        for p in 1..packetLen:
+            let next = processPacket(input, pStart)
+            packetVals.add(next.cumulativeValue)
+            pStart = next.nextIndex
+
+        let response = processValues(packetVals, typeId)
+        return ParsePacketResponse(nextIndex: pStart, cumulativeValue: response, shouldStop:true)
+
+    return ParsePacketResponse(nextIndex: 0, cumulativeValue: 0, shouldStop:true)
+
+proc process(input: string): int =
+    var i = 0
+    var resp = 0
+    let bitInput = toBits(input)
+    while true:
+        let changes = processPacket(bitInput, i)
+        resp += changes.cumulativeValue
+        i = changes.nextIndex
+        if changes.nextIndex >= bitInput.len() or changes.shouldStop:
+            break
+    return resp
+
+let input = getInput()
+let resp = process(input)
+echo(resp)
+
