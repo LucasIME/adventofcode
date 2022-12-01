8f409c77811a6bfbcffa3470a93a2b1ea5365734
Lucas Meireles <meireles31@msn.com>
2022-12-01 11:14:03 +0300

    Added solution to 2022 Q1

---
+++ 2022/AdventOfCode/src/main/kotlin/q1/q1.kt
@@ -0,0 +1,30 @@
+package q1
+
+import java.io.File
+import kotlin.math.max
+
+fun main() {
+    val input = parseInput()
+    println(process(input))
+}
+
+private fun process(input: List<String>): Long {
+    var maxSoFar = Long.MIN_VALUE;
+    var curMax = Long.MIN_VALUE;
+
+    input.forEach{
+        if (it == "") {
+            maxSoFar = max(maxSoFar, curMax)
+            curMax = 0
+        } else {
+            curMax += Integer.parseInt(it)
+        }
+    }
+
+    return maxSoFar
+}
+
+private fun parseInput(): List<String> {
+    val f = File("src/main/resources/q1.txt")
+    return f.readLines()
+}
