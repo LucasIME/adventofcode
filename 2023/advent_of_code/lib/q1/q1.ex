fcd7b4bf9c86f9ab23fd0e4420b2b4455f6135ac
Lucas Meireles <meireles31@msn.com>
2023-12-02 08:18:22 +0000

    formatting q1

--- 2023/advent_of_code/lib/q1/q1.ex
+++ 2023/advent_of_code/lib/q1/q1.ex
@@ -28,18 +28,18 @@ defmodule AdventOfCode.Q1 do
 
   def toDigitsWithWords(s, result \\ []) do
     case s do
-      ["1" | tail] -> toDigitsWithWords(tail, [1  | result])
-      ["2" | tail] -> toDigitsWithWords(tail, [2  | result])
-      ["3" | tail] -> toDigitsWithWords(tail, [3  | result])
-      ["4" | tail] -> toDigitsWithWords(tail, [4  | result])
-      ["5" | tail] -> toDigitsWithWords(tail, [5  | result])
-      ["6" | tail] -> toDigitsWithWords(tail, [6  | result])
-      ["7" | tail] -> toDigitsWithWords(tail, [7  | result])
-      ["8" | tail] -> toDigitsWithWords(tail, [8  | result])
-      ["9" | tail] -> toDigitsWithWords(tail, [9  | result])
-      ["o", "n", "e" | _tail] -> toDigitsWithWords(List.delete_at(s, 0), [1  | result])
-      ["t", "w", "o" | _tail] -> toDigitsWithWords(List.delete_at(s, 0), [2  | result])
-      ["t", "h", "r", "e", "e" | _tail] -> toDigitsWithWords(List.delete_at(s, 0), [3  | result])
+      ["1" | tail] -> toDigitsWithWords(tail, [1 | result])
+      ["2" | tail] -> toDigitsWithWords(tail, [2 | result])
+      ["3" | tail] -> toDigitsWithWords(tail, [3 | result])
+      ["4" | tail] -> toDigitsWithWords(tail, [4 | result])
+      ["5" | tail] -> toDigitsWithWords(tail, [5 | result])
+      ["6" | tail] -> toDigitsWithWords(tail, [6 | result])
+      ["7" | tail] -> toDigitsWithWords(tail, [7 | result])
+      ["8" | tail] -> toDigitsWithWords(tail, [8 | result])
+      ["9" | tail] -> toDigitsWithWords(tail, [9 | result])
+      ["o", "n", "e" | _tail] -> toDigitsWithWords(List.delete_at(s, 0), [1 | result])
+      ["t", "w", "o" | _tail] -> toDigitsWithWords(List.delete_at(s, 0), [2 | result])
+      ["t", "h", "r", "e", "e" | _tail] -> toDigitsWithWords(List.delete_at(s, 0), [3 | result])
       ["f", "o", "u", "r" | _tail] -> toDigitsWithWords(List.delete_at(s, 0), [4 | result])
       ["f", "i", "v", "e" | _tail] -> toDigitsWithWords(List.delete_at(s, 0), [5 | result])
       ["s", "i", "x" | _tail] -> toDigitsWithWords(List.delete_at(s, 0), [6 | result])
@@ -47,7 +47,7 @@ defmodule AdventOfCode.Q1 do
       ["e", "i", "g", "h", "t" | _tail] -> toDigitsWithWords(List.delete_at(s, 0), [8 | result])
       ["n", "i", "n", "e" | _tail] -> toDigitsWithWords(List.delete_at(s, 0), [9 | result])
       [] -> Enum.reverse(result)
-      [_| tail] -> toDigitsWithWords(tail, result)
+      [_ | tail] -> toDigitsWithWords(tail, result)
     end
   end
 
