7be8230545bae1def1d7ee1f5cb9c0b69edf8940
Lucas Meireles <meireles31@msn.com>
2023-12-07 09:42:27 +0000

    Added solution to 2023 Q7 part2

--- 2023/advent_of_code/lib/q7/q7.ex
+++ 2023/advent_of_code/lib/q7/q7.ex
@@ -18,20 +18,58 @@ defmodule AdventOfCode.Q7 do
     end
   end
 
+  def process_joker(%{"J" => 5}) do
+    %{"J" => 5}
+  end
+
+  def process_joker(card_freq) do
+    if Map.has_key?(card_freq, "J") do
+      j_freq = card_freq["J"]
+      card_freq = Map.delete(card_freq, "J")
+
+      {card_with_highest_freq, freq} = card_freq |> Enum.max_by(fn {_card, freq} -> freq end)
+      card_freq = Map.put(card_freq, card_with_highest_freq, freq + j_freq)
+
+      card_freq
+    else
+      card_freq
+    end
+  end
+
+  def card_freq_to_points2(card_freq) do
+    freq_after_joker = process_joker(card_freq)
+    card_freq_to_points(freq_after_joker)
+  end
+
   @card_power_order ~w(A K Q J T 9 8 7 6 5 4 3 2)
   @card_to_power @card_power_order
                  |> Enum.reverse()
                  |> Enum.with_index()
                  |> Enum.into(%{})
+  @card_power_order2 ~w(A K Q T 9 8 7 6 5 4 3 2 J)
+  @card_to_power2 @card_power_order2
+                  |> Enum.reverse()
+                  |> Enum.with_index()
+                  |> Enum.into(%{})
 
   def card_to_val(card) do
     @card_to_power[card]
+  end
+
+  def card_to_val2(card) do
+    @card_to_power2[card]
   end
 
   def hand_to_card_power_array(hand) do
     hand
     |> String.graphemes()
     |> Enum.map(&card_to_val/1)
+  end
+
+  def hand_to_card_power_array2(hand) do
+    hand
+    |> String.graphemes()
+    |> Enum.map(&card_to_val2/1)
   end
 
   def compare_hands(hand1, hand2) do
@@ -46,11 +84,33 @@ defmodule AdventOfCode.Q7 do
     {hand1_points, hand_array} < {hand2_points, hand2_array}
   end
 
+  def compare_hands2(hand1, hand2) do
+    hand1_freq = hand1 |> String.graphemes() |> Enum.frequencies()
+    hand1_points = card_freq_to_points2(hand1_freq)
+    hand_array = hand_to_card_power_array2(hand1)
+
+    hand2_freq = hand2 |> String.graphemes() |> Enum.frequencies()
+    hand2_points = card_freq_to_points2(hand2_freq)
+    hand2_array = hand_to_card_power_array2(hand2)
+
+    {hand1_points, hand_array} < {hand2_points, hand2_array}
+  end
+
   def part1(input \\ IO.stream(:stdio, :line)) do
     input
     |> Enum.map(&String.trim/1)
     |> Enum.map(&to_hand_and_bid/1)
     |> Enum.sort(fn {hand1, _bid1}, {hand2, _bid2} -> compare_hands(hand1, hand2) end)
+    |> Enum.with_index(1)
+    |> Enum.map(fn {{_hand, bid}, index} -> bid * index end)
+    |> Enum.sum()
+  end
+
+  def part2(input \\ IO.stream(:stdio, :line)) do
+    input
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&to_hand_and_bid/1)
+    |> Enum.sort(fn {hand1, _bid1}, {hand2, _bid2} -> compare_hands2(hand1, hand2) end)
     |> Enum.with_index(1)
     |> Enum.map(fn {{_hand, bid}, index} -> bid * index end)
     |> Enum.sum()
--- 2023/advent_of_code/test/q7/q7_test.exs
+++ 2023/advent_of_code/test/q7/q7_test.exs
@@ -13,4 +13,16 @@ defmodule AdventOfCode.Q7Test do
     result = AdventOfCode.Q7.part1(file_stream)
     assert result == 241_344_943
   end
+
+  test "Q7 Part 2 Example works" do
+    file_stream = File.stream!("./test/q7/q7_example.txt")
+    result = AdventOfCode.Q7.part2(file_stream)
+    assert result == 5905
+  end
+
+  test "Q7 Part 2 works" do
+    file_stream = File.stream!("./test/q7/q7.txt")
+    result = AdventOfCode.Q7.part2(file_stream)
+    assert result == 243_101_568
+  end
 end
