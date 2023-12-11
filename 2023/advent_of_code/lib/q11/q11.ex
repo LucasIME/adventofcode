7cd282885dc74760a6c3d1dfe38af056be946a37
Lucas Meireles <meireles31@msn.com>
2023-12-11 07:46:51 -0300

    Added solution to 2023 Q11 part2 and improved solution for part1

--- 2023/advent_of_code/lib/q11/q11.ex
+++ 2023/advent_of_code/lib/q11/q11.ex
@@ -7,29 +7,10 @@ defmodule AdventOfCode.Q11 do
     c == "." and all_empty(tail)
   end
 
-  def expand_rows([]) do
-    []
-  end
-
-  def expand_rows([row | tail]) do
-    if all_empty(row) do
-      [row, row | expand_rows(tail)]
-    else
-      [row | expand_rows(tail)]
-    end
-  end
-
   def transpose(matrix) do
     matrix
     |> Enum.zip()
     |> Enum.map(&Tuple.to_list/1)
-  end
-
-  def expand_cols(universe) do
-    universe
-    |> transpose()
-    |> expand_rows()
-    |> transpose()
   end
 
   def get_points([], out, _row, _col) do
@@ -55,11 +36,62 @@ defmodule AdventOfCode.Q11 do
     get_points(matrix, [], 0, 0)
   end
 
-  def get_every_pair_dist(points) do
+  def get_empty_rows([], out, _row) do
+    out
+  end
+
+  def get_empty_rows([line | tail], out, row) when is_list(line) do
+    new_out =
+      case all_empty(line) do
+        true -> MapSet.put(out, row)
+        false -> out
+      end
+
+    get_empty_rows(tail, new_out, row + 1)
+  end
+
+  def get_empty_rows(matrix) do
+    get_empty_rows(matrix, MapSet.new(), 0)
+  end
+
+  def get_empty_rows_and_cols(universe) do
+    empty_rows = get_empty_rows(universe)
+    empty_cols = get_empty_rows(universe |> transpose())
+
+    {empty_rows, empty_cols}
+  end
+
+  def get_every_pair_dist(points, empty_rows, empty_cols, multiplier) do
     all_pairs = for p1 <- points, p2 <- points, do: {p1, p2}
 
     all_pairs
-    |> Enum.map(fn {{row1, col1}, {row2, col2}} -> abs(row2 - row1) + abs(col2 - col1) end)
+    |> Enum.map(fn {{row1, col1}, {row2, col2}} ->
+      result = abs(row2 - row1) + abs(col2 - col1)
+      min_row = min(row1, row2)
+      max_row = max(row1, row2)
+      min_col = min(col1, col2)
+      max_col = max(col1, col2)
+
+      result =
+        Enum.reduce(empty_cols, result, fn col, acc ->
+          if col > min_col and col < max_col do
+            acc + (multiplier - 1)
+          else
+            acc
+          end
+        end)
+
+      result =
+        Enum.reduce(empty_rows, result, fn row, acc ->
+          if row > min_row and row < max_row do
+            acc + (multiplier - 1)
+          else
+            acc
+          end
+        end)
+
+      result
+    end)
   end
 
   def part1(input \\ IO.stream(:stdio, :line)) do
@@ -68,13 +100,24 @@ defmodule AdventOfCode.Q11 do
       |> Enum.map(&String.trim/1)
       |> Enum.map(&String.graphemes/1)
 
-    expanded_universe =
-      universe
-      |> expand_rows()
-      |> expand_cols()
+    {empty_rows, empty_cols} = get_empty_rows_and_cols(universe)
+    points = get_points(universe)
+    every_pair_dist = get_every_pair_dist(points, empty_rows, empty_cols, 2)
 
-    points = get_points(expanded_universe)
-    every_pair_dist = get_every_pair_dist(points)
+    every_pair_dist
+    |> Enum.sum()
+    |> div(2)
+  end
+
+  def part2(input \\ IO.stream(:stdio, :line), multiplier \\ 1_000_000) do
+    universe =
+      input
+      |> Enum.map(&String.trim/1)
+      |> Enum.map(&String.graphemes/1)
+
+    {empty_rows, empty_cols} = get_empty_rows_and_cols(universe)
+    points = get_points(universe)
+    every_pair_dist = get_every_pair_dist(points, empty_rows, empty_cols, multiplier)
 
     every_pair_dist
     |> Enum.sum()
--- 2023/advent_of_code/test/q11/q11_test.exs
+++ 2023/advent_of_code/test/q11/q11_test.exs
@@ -13,4 +13,22 @@ defmodule AdventOfCode.Q11Test do
     result = AdventOfCode.Q11.part1(file_stream)
     assert result == 9_734_203
   end
+
+  test "Q11 Part 2 Example works" do
+    file_stream = File.stream!("./test/q11/q11_example.txt")
+    result = AdventOfCode.Q11.part2(file_stream, 10)
+    assert result == 1030
+  end
+
+  test "Q11 Part 2 Example 2 works" do
+    file_stream = File.stream!("./test/q11/q11_example.txt")
+    result = AdventOfCode.Q11.part2(file_stream, 100)
+    assert result == 8410
+  end
+
+  test "Q11 Part 2 works" do
+    file_stream = File.stream!("./test/q11/q11.txt")
+    result = AdventOfCode.Q11.part2(file_stream)
+    assert result == 568_914_596_391
+  end
 end
