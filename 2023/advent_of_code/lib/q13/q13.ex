6f6fefba8a69085189334ec0e0d5e115e8e05ce6
Lucas Meireles <meireles31@msn.com>
2023-12-13 10:13:03 -0300

    Added solution to 2023 Q13 part2

--- 2023/advent_of_code/lib/q13/q13.ex
+++ 2023/advent_of_code/lib/q13/q13.ex
@@ -30,7 +30,7 @@ defmodule AdventOfCode.Q13 do
   end
 
   def get_points(grid) do
-    if grid == Enum.reverse(grid) do
+    if rem(length(grid), 2) == 0 and grid == Enum.reverse(grid) do
       {div(length(grid), 2), true}
     else
       {points, is_match} = get_points(tl(grid))
@@ -75,6 +75,22 @@ defmodule AdventOfCode.Q13 do
     end
   end
 
+  def all_summaries(grid) do
+    {rowp, row_ok} = get_points(grid)
+    row = {100 * rowp, row_ok}
+
+    col = get_points(transpose(grid))
+
+    {rev_rowp, rev_row_ok} = rev_get_points(grid |> Enum.reverse())
+    rev_row = {100 * rev_rowp, rev_row_ok}
+
+    rev_col = rev_get_points(transpose(grid) |> Enum.reverse())
+
+    [row, col, rev_row, rev_col]
+    |> Enum.filter(fn {_p, ok?} -> ok? end)
+    |> Enum.map(fn {p, _} -> p end)
+  end
+
   def part1(input \\ IO.stream(:stdio, :line)) do
     grid_list =
       input
@@ -97,6 +113,83 @@ defmodule AdventOfCode.Q13 do
 
     grid_list
     |> Enum.map(&summarize/1)
+    |> Enum.sum()
+  end
+
+  def invert_c(c) do
+    case c do
+      "#" -> "."
+      "." -> "#"
+    end
+  end
+
+  def invert(matrix, row, col) do
+    tuple_matrix = matrix |> Enum.map(&List.to_tuple/1) |> List.to_tuple()
+
+    tuple_matrix =
+      put_elem(
+        tuple_matrix,
+        row,
+        put_elem(tuple_matrix |> elem(row), col, invert_c(tuple_matrix |> elem(row) |> elem(col)))
+      )
+
+    Tuple.to_list(tuple_matrix) |> Enum.map(&Tuple.to_list/1)
+  end
+
+  def all_variations(matrix) do
+    all_modified =
+      for row <- 0..(length(matrix) - 1),
+          col <- 0..(length(hd(matrix)) - 1),
+          do: invert(matrix, row, col)
+
+    all_modified
+  end
+
+  def summarize_different(grid) do
+    original_val = summarize(grid)
+
+    resp =
+      grid
+      |> all_variations()
+      |> Enum.flat_map(fn matrix ->
+        summaries = all_summaries(matrix)
+
+        filtered_summaries =
+          summaries
+          |> Enum.filter(fn matrix_val ->
+            matrix_val != original_val and matrix_val != 0
+          end)
+
+        filtered_summaries
+      end)
+
+    # Not sure why I have to take the max and not just any? In theory these were not supposed to be different...
+    resp
+    |> Enum.max()
+  end
+
+  def part2(input \\ IO.stream(:stdio, :line)) do
+    grid_list =
+      input
+      |> Enum.map(&String.trim/1)
+      |> Enum.chunk_while(
+        [],
+        fn line, acc ->
+          if line == "" do
+            {:cont, Enum.reverse(acc), []}
+          else
+            {:cont, [line | acc]}
+          end
+        end,
+        fn
+          [] -> {:cont, []}
+          acc -> {:cont, Enum.reverse(acc), []}
+        end
+      )
+      |> Enum.map(fn grid -> Enum.map(grid, &String.graphemes/1) end)
+
+    grid_list
+    |> Enum.map(&summarize_different/1)
     |> Enum.sum()
   end
 end
--- 2023/advent_of_code/test/q13/q13_test.exs
+++ 2023/advent_of_code/test/q13/q13_test.exs
@@ -13,4 +13,16 @@ defmodule AdventOfCode.Q13Test do
     result = AdventOfCode.Q13.part1(file_stream)
     assert result == 39939
   end
+
+  test "Q13 Part 2 Example works" do
+    file_stream = File.stream!("./test/q13/q13_example.txt")
+    result = AdventOfCode.Q13.part2(file_stream)
+    assert result == 400
+  end
+
+  test "Q13 Part 2 works" do
+    file_stream = File.stream!("./test/q13/q13.txt")
+    result = AdventOfCode.Q13.part2(file_stream)
+    assert result == 32069
+  end
 end
