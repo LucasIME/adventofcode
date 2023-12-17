4d9f2db72ff9db540af6a63a9a0570238f78ae20
Lucas Meireles <meireles31@msn.com>
2023-12-17 17:45:56 -0300

    Added elixir solution to 2023 Q17 part1 with priority queue

--- 2023/advent_of_code/lib/q17/q17.ex
+++ 2023/advent_of_code/lib/q17/q17.ex
@@ -1,13 +1,17 @@
 defmodule AdventOfCode.Q17 do
   def is_valid?(grid, row, col) do
-    row >=0 and row < tuple_size(grid) and col >=0 and col < tuple_size(grid |> elem(row))
+    row >= 0 and row < tuple_size(grid) and col >= 0 and col < tuple_size(grid |> elem(row))
   end
 
   def initialize_distances(grid) do
     last_row = tuple_size(grid) - 1
     last_col = tuple_size(grid |> elem(0)) - 1
 
-    cells = for row <- 0..last_row, col <- 0..last_col, dir <- [:up, :down, :left, :right], do: {row, col, dir}
+    cells =
+      for row <- 0..last_row,
+          col <- 0..last_col,
+          dir <- [:up, :down, :left, :right],
+          do: {row, col, dir}
 
     dists = cells |> Enum.map(fn pos -> {pos, :infinity} end) |> Enum.into(%{})
     dists = Map.put(dists, {0, 0, :left}, 0)
@@ -15,18 +19,20 @@ defmodule AdventOfCode.Q17 do
   end
 
   def find_min_distance(unvisited, distances) do
-    unvisited |> MapSet.to_list() |> Enum.map(fn node -> {node, Map.get(distances, node)} end)
+    unvisited
+    |> MapSet.to_list()
+    |> Enum.map(fn node -> {node, Map.get(distances, node)} end)
     |> Enum.min_by(fn {_node, distance} -> distance end)
   end
 
-  def update_distances(distances, neigh_with_dist) do
-    Enum.reduce(neigh_with_dist, distances, fn {neigh, new_dist}, acc_dist ->
+  def update_distances(pq, distances, neigh_with_dist) do
+    Enum.reduce(neigh_with_dist, {pq, distances}, fn {neigh, new_dist}, {acc_pq, acc_dist} ->
       old_dist = Map.get(acc_dist, neigh)
 
       if new_dist < old_dist do
-        Map.put(acc_dist, neigh, new_dist)
+        {PriorityQueue.put(acc_pq, {new_dist, neigh}), Map.put(acc_dist, neigh, new_dist)}
       else
-        acc_dist
+        {acc_pq, acc_dist}
       end
     end)
   end
@@ -45,11 +51,15 @@ defmodule AdventOfCode.Q17 do
   end
 
   def get_dist_after_steps(grid, row, col, dir, cur_dist, steps) do
-    {next_row, next_col}= get_next(row, col, dir)
+    {next_row, next_col} = get_next(row, col, dir)
 
     if is_valid?(grid, next_row, next_col) do
       new_dist = cur_dist + (grid |> elem(next_row) |> elem(next_col))
-      [{{next_row, next_col, dir}, new_dist} | get_dist_after_steps(grid, next_row, next_col, dir, new_dist, steps - 1)]
+
+      [
+        {{next_row, next_col, dir}, new_dist}
+        | get_dist_after_steps(grid, next_row, next_col, dir, new_dist, steps - 1)
+      ]
     else
       []
     end
@@ -69,38 +79,39 @@ defmodule AdventOfCode.Q17 do
     possible_dirs |> Enum.flat_map(&get_dist_after_steps(grid, row, col, &1, cur_dist, 3))
   end
 
-  def find_shortest(grid, unvisited, distances, target) do
-    {{row, col, dir}, cur_dist} = find_min_distance(unvisited, distances)
+  def find_shortest(grid, distances, priority_queue, target) do
+    {{cur_dist, {row, col, dir}}, new_priority_queue} = PriorityQueue.pop!(priority_queue)
 
     if {row, col} == target do
       cur_dist
     else
       neigh_with_dist = get_neighs(grid, row, col, dir, cur_dist)
 
-      updated_distances = update_distances(distances, neigh_with_dist)
-      unvisited = MapSet.delete(unvisited, {row, col, dir})
+      {new_priority_queue, updated_distances} =
+        update_distances(new_priority_queue, distances, neigh_with_dist)
 
-      find_shortest(grid, unvisited, updated_distances, target)
+      find_shortest(grid, updated_distances, new_priority_queue, target)
     end
   end
 
   def min_dist(grid) do
     distances = initialize_distances(grid)
-    unsivited = Map.keys(distances) |> MapSet.new()
 
     target = {tuple_size(grid) - 1, tuple_size(grid |> elem(0)) - 1}
+    priority_queue = [{0, {0, 0, :left}}, {0, {0, 0, :up}}] |> Enum.into(PriorityQueue.new())
 
-    find_shortest(grid, unsivited, distances, target)
+    find_shortest(grid, distances, priority_queue, target)
   end
 
   def part1(input \\ IO.stream(:stdio, :line)) do
-      grid = input
+    grid =
+      input
       |> Enum.map(&String.trim/1)
       |> Enum.map(&String.graphemes/1)
       |> Enum.map(&Enum.map(&1, fn char -> String.to_integer(char) end))
       |> Enum.map(&List.to_tuple/1)
       |> List.to_tuple()
 
-      min_dist(grid)
+    min_dist(grid)
   end
 end
