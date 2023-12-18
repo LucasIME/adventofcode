e3a26dfa94d49f224c1fac5f4b570130eb3dd27d
Lucas Meireles <meireles31@msn.com>
2023-12-18 08:46:52 -0300

    Added solution to 2023 Q18 part2

--- 2023/advent_of_code/lib/q18/q18.ex
+++ 2023/advent_of_code/lib/q18/q18.ex
@@ -11,7 +11,7 @@ defmodule AdventOfCode.Q18 do
   def to_vertices(steps) do
     initial_vertice = [{0, 0}]
 
-    Enum.reduce(steps, initial_vertice, fn {dir, steps, _color}, acc_vertices ->
+    Enum.reduce(steps, initial_vertice, fn {dir, steps}, acc_vertices ->
       [{last_row, last_col} | _tail] = acc_vertices
       next_vertice = get_next(last_row, last_col, dir, steps)
       [next_vertice | acc_vertices]
@@ -53,7 +53,7 @@ defmodule AdventOfCode.Q18 do
   def to_outline(steps) do
     initial_vertice = [{0, 0}]
 
-    Enum.reduce(steps, initial_vertice, fn {dir, steps, _color}, acc_vertices ->
+    Enum.reduce(steps, initial_vertice, fn {dir, steps}, acc_vertices ->
       [{last_row, last_col} | _tail] = acc_vertices
       next_vertices = get_next_n(last_row, last_col, dir, steps)
       Enum.reverse(next_vertices) ++ acc_vertices
@@ -65,7 +65,48 @@ defmodule AdventOfCode.Q18 do
       input
       |> Enum.map(&String.trim/1)
       |> Enum.map(&String.split(&1, " "))
-      |> Enum.map(fn [dir, raw_steps, color] -> {dir, String.to_integer(raw_steps), color} end)
+      |> Enum.map(fn [dir, raw_steps, _color] -> {dir, String.to_integer(raw_steps)} end)
+
+    outline = to_outline(steps)
+    # first point double counted
+    perimeter = length(outline) - 1
+
+    vertices = to_vertices(steps)
+    vertices = Enum.drop(vertices, 1)
+
+    shoelace = shoelace(vertices)
+
+    # Pick's theorem
+    area = (shoelace + div(perimeter, 2) + 1) |> trunc()
+    area
+  end
+
+  def color_number_to_direction(color_number)  do
+    case color_number do
+      "0" -> "R"
+      "1" -> "D"
+      "2" -> "L"
+      "3" -> "U"
+    end
+  end
+
+  def color_to_instruction(color_with_parens) do
+    color_numbers = color_with_parens |> String.graphemes |>  Enum.drop(2) # dropping (#
+    reversed_color_numbers = color_numbers |> Enum.reverse() |> Enum.drop(1)
+    [direction_color_number | remaining_numbers] = reversed_color_numbers
+
+    raw_hex = remaining_numbers |> Enum.reverse() |> List.to_string()
+    real_hex = String.to_integer(raw_hex, 16)
+
+    {color_number_to_direction(direction_color_number), real_hex}
+  end
+
+  def part2(input \\ IO.stream(:stdio, :line)) do
+    steps =
+      input
+      |> Enum.map(&String.trim/1)
+      |> Enum.map(&String.split(&1, " "))
+      |> Enum.map(fn [_dir, _raw_steps, color] -> color_to_instruction(color) end)
 
     outline = to_outline(steps)
     # first point double counted
--- 2023/advent_of_code/test/q18/q18_test.exs
+++ 2023/advent_of_code/test/q18/q18_test.exs
@@ -13,4 +13,16 @@ defmodule AdventOfCode.Q18Test do
     result = AdventOfCode.Q18.part1(file_stream)
     assert result == 26857
   end
+
+  test "Q18 Part 2 Example works" do
+    file_stream = File.stream!("./test/q18/q18_example.txt")
+    result = AdventOfCode.Q18.part2(file_stream)
+    assert result == 952408144115
+  end
+
+  test "Q18 Part 2 works" do
+    file_stream = File.stream!("./test/q18/q18.txt")
+    result = AdventOfCode.Q18.part2(file_stream)
+    assert result == 129373230496292
+  end
 end
