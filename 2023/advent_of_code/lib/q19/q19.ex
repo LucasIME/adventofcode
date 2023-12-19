45623dc810d5f923769fa9ff5938ae36249f9635
Lucas Meireles <meireles31@msn.com>
2023-12-19 13:57:09 -0300

    Added solution to 2023 Q19 part2

--- 2023/advent_of_code/lib/q19/q19.ex
+++ 2023/advent_of_code/lib/q19/q19.ex
@@ -101,4 +101,204 @@ defmodule AdventOfCode.Q19 do
     |> Enum.flat_map(&Map.values/1)
     |> Enum.sum()
   end
+
+  @default_range 1..4000
+  @all_match %{
+    "x" => [@default_range],
+    "m" => [@default_range],
+    "a" => [@default_range],
+    "s" => [@default_range]
+  }
+
+  def get_exclude_guard(guard) do
+    key = String.at(guard, 0)
+    operator = String.at(guard, 1)
+    value = String.slice(guard, 2..-1//1) |> String.to_integer()
+
+    case operator do
+      ">" -> %{key => [(value + 1)..4000]}
+      "<" -> %{key => [1..(value - 1)]}
+    end
+  end
+
+  def get_guard_range(guard) do
+    key = String.at(guard, 0)
+    operator = String.at(guard, 1)
+    value = String.slice(guard, 2..-1//1) |> String.to_integer()
+
+    new_key =
+      case operator do
+        ">" -> %{key => [(value + 1)..4000]}
+        "<" -> %{key => [1..(value - 1)]}
+      end
+
+    Map.merge(@all_match, new_key)
+  end
+
+  def merge_ranges(ranges) do
+    sorted_ranges = Enum.sort(ranges, fn a, b -> a.first < b.first end)
+
+    Enum.reduce(sorted_ranges, [], fn current_range, acc ->
+      case acc do
+        [] ->
+          [current_range]
+
+        [previous_range | tail] ->
+          if current_range.first - 1 <= previous_range.last do
+            [previous_range.first..max(previous_range.last, current_range.last) | tail]
+          else
+            [current_range | acc]
+          end
+      end
+    end)
+    |> Enum.reverse()
+  end
+
+  def union_key(key, m1, m2) do
+    ranges1 = Map.get(m1, key, [])
+    ranges2 = Map.get(m2, key, [])
+    merge_ranges(ranges1 ++ ranges2)
+  end
+
+  def union(intervals1, intervals2) do
+    keys = ["x", "m", "a", "s"]
+
+    keys
+    |> Enum.map(fn key -> {key, union_key(key, intervals1, intervals2)} end)
+    |> Enum.into(%{})
+  end
+
+  def intersect_ranges(range_list1, range_list2) do
+    range_list1
+    |> Enum.flat_map(fn r1 ->
+      Enum.map(range_list2, fn r2 -> find_intersection(r1, r2) end)
+    end)
+    |> Enum.filter(& &1)
+  end
+
+  defp find_intersection(%Range{first: a, last: b}, %Range{first: x, last: y}) do
+    if y < a || x > b do
+      nil
+    else
+      first = max(a, x)
+      last = min(b, y)
+      first..last
+    end
+  end
+
+  def inter_key(key, m1, m2) do
+    ranges1 = Map.get(m1, key, [])
+    ranges2 = Map.get(m2, key, [])
+    intersect_ranges(ranges1, ranges2)
+  end
+
+  def intersection(intervals1, intervals2) do
+    keys = ["x", "m", "a", "s"]
+
+    keys
+    |> Enum.map(fn key -> {key, inter_key(key, intervals1, intervals2)} end)
+    |> Enum.into(%{})
+  end
+
+  def matching_intervals(
+        rule,
+        rules,
+        excluding_intervals \\ %{"x" => [], "m" => [], "a" => [], "s" => []}
+      )
+
+  def matching_intervals([{guard, target} | rule_tail], rules, excluding_intervals) do
+    guard_range = get_guard_range(guard)
+
+    matching_points =
+      case target do
+        "A" ->
+          [guard_range]
+
+        "R" ->
+          []
+
+        other_rule ->
+          other_rule_match = matching_intervals(rules[other_rule], rules, excluding_intervals)
+          other_rule_match |> Enum.map(&intersection(guard_range, &1))
+      end
+
+    my_match = matching_points |> Enum.map(fn point -> difference(point, excluding_intervals) end)
+
+    new_exclude = union(get_exclude_guard(guard), excluding_intervals)
+    my_match ++ matching_intervals(rule_tail, rules, new_exclude)
+  end
+
+  def matching_intervals([last_guard], rules, excluding_intervals) do
+    case last_guard do
+      {"A"} -> [difference(@all_match, excluding_intervals)]
+      {"R"} -> []
+      {other_rule} -> matching_intervals(rules[other_rule], rules, excluding_intervals)
+    end
+  end
+
+  def difference(intervals1, intervals2) do
+    keys = ["x", "m", "a", "s"]
+
+    keys
+    |> Enum.map(fn key -> {key, diff_key(key, intervals1, intervals2)} end)
+    |> Enum.into(%{})
+  end
+
+  def diff_key(key, m1, m2) do
+    ranges1 = Map.get(m1, key, [])
+    ranges2 = Map.get(m2, key, [])
+    difference_ranges(ranges1, ranges2)
+  end
+
+  def difference_ranges(range_list1, range_list2) do
+    range_list1
+    |> Enum.flat_map(fn r1 ->
+      Enum.reduce(range_list2, [r1], fn r2, acc ->
+        Enum.flat_map(acc, &subtract_ranges(&1, r2))
+      end)
+    end)
+  end
+
+  defp subtract_ranges(%Range{first: a, last: b}, %Range{first: x, last: y}) do
+    cond do
+      y < a || x > b ->
+        [%Range{first: a, last: b, step: 1}]
+
+      x <= a && y >= b ->
+        []
+
+      x <= a && y < b ->
+        [Range.new(y + 1, b)]
+
+      x > a && y >= b ->
+        [Range.new(a, x - 1)]
+
+      x > a && y < b ->
+        [Range.new(a, x - 1), Range.new(y + 1, b)]
+    end
+  end
+
+  def interval_total([]) do
+    0
+  end
+
+  def interval_total(interval) do
+    interval
+    |> Map.values()
+    |> Enum.map(fn [range] -> Range.size(range) end)
+    |> Enum.product()
+  end
+
+  def part2(input \\ IO.stream(:stdio, :line)) do
+    [raw_rules | _] =
+      input
+      |> Enum.map(&String.trim/1)
+      |> split_by_empty_line()
+
+    rules = raw_rules |> parse_rules()
+
+    matching_intervals(Map.get(rules, "in"), rules)
+    |> Enum.map(&interval_total/1)
+    |> Enum.sum()
+  end
 end
--- 2023/advent_of_code/test/q19/q19_test.exs
+++ 2023/advent_of_code/test/q19/q19_test.exs
@@ -11,6 +11,18 @@ defmodule AdventOfCode.Q19Test do
   test "Q19 Part 1 works" do
     file_stream = File.stream!("./test/q19/q19.txt")
     result = AdventOfCode.Q19.part1(file_stream)
-    assert result == 26857
+    assert result == 332_145
+  end
+
+  test "Q19 Part 2 Example works" do
+    file_stream = File.stream!("./test/q19/q19_example.txt")
+    result = AdventOfCode.Q19.part2(file_stream)
+    assert result == 167_409_079_868_000
+  end
+
+  test "Q19 Part 2 works" do
+    file_stream = File.stream!("./test/q19/q19.txt")
+    result = AdventOfCode.Q19.part2(file_stream)
+    assert result == 136_661_579_897_555
   end
 end
