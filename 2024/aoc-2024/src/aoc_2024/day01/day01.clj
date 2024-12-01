146a4e65371c42b5f764ed1a89173ac903c32089
Lucas Meireles <lmeireles@Lucass-MacBook-Pro-2.local>
2024-12-01 10:13:52 +0000

    Added solution to 2024 day01 part 1

--- 2024/aoc-2024/src/aoc_2024/day01/day01.clj
+++ 2024/aoc-2024/src/aoc_2024/day01/day01.clj
@@ -1,18 +1,29 @@
 (ns aoc-2024.day01.day01
-  (:require [aoc-2024.utils.utils :as utils]))
+  (:require [aoc-2024.utils.utils :as utils]
+             [clojure.string :as str]))
 
-(defn part1 []
-  (let [file "day01/part1.txt"
-        lines (utils/read-file file)]
-        lines
-    ))
 
-(defn part2 []
-  (let [file "day01/part2.txt"
-        lines (utils/read-file file)
-        intLines (map #(Integer/parseInt %) lines)]
-        (reduce + intLines)
-    ))
+(defn parse-input [lines]
+  (let [paired_lines (map #(str/split %1 #"\s+") lines)
+        l1 (map #(Integer/parseInt (first %1)) paired_lines)
+        l2 (map #(Integer/parseInt (second %1)) paired_lines)
+        resp (list l1 l2)] 
+    resp))
 
-;; (doseq [line (part1)] 
-;;   (println line))
+(defn solve [[l1 l2]]
+  (let [sorted1 (sort l1)
+        sorted2 (sort l2)
+        zipped (map vector sorted1 sorted2)
+        diff (map (fn [[v1 v2]] (Math/abs (- v2 v1))) zipped)
+        resp (reduce + diff)]
+    resp))
+
+(defn part1 
+  ([] (part1 "day01/part1.txt"))
+  ([fileName]
+    (let [file fileName
+          lines (utils/read-file file)
+          input (parse-input lines)
+          resp (solve input)]
+          resp
+      )))
--- 2024/aoc-2024/test/aoc_2024/day01/day01_test.clj
+++ 2024/aoc-2024/test/aoc_2024/day01/day01_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [aoc-2024.day01.day01 :refer :all]))
 
-((deftest day01-part2-test
-      (testing "Day 01 part2 passes"
-        (is (= 103 (part2))))) )
+(deftest day01-ex1-test
+      (testing "Day 01 ex1"
+        (is (= 11 (part1 "day01/ex1.txt")))))
+
+(deftest day01-part1-test
+      (testing "Day 01 part1"
+        (is (= 2815556 (part1)))))
