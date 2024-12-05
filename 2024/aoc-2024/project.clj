91e9159fcf162dcec4cefc33e81e14c5babd6fd1
Lucas Meireles <meireles31@msn.com>
2024-12-05 08:44:12 +0000

    using clojure 1.11

--- 2024/aoc-2024/project.clj
+++ 2024/aoc-2024/project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/lucasime/adventofcode"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.11.0"]]
   :repl-options {:init-ns aoc-2024.core})
