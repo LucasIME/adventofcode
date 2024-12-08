f850606cbb4c83033eb4eeff17783e417e28fbdf
Lucas Meireles <meireles31@msn.com>
2024-12-08 10:11:31 +0000

    Added solution to 2024 Q8 part2

--- 2024/aoc-2024/src/aoc_2024/day08/day08.clj
+++ 2024/aoc-2024/src/aoc_2024/day08/day08.clj
@@ -54,3 +54,31 @@
       (utils/read-file-lines)
       (parse-input)
       (solve))))
+
+(defn get-in-direction [[row col] [v-row v-col] matrix]
+  (->> (iterate (fn [[r c]] [(+ r v-row) (+ c v-col)]) [row col])
+       (take-while #(is-inside? % matrix))))
+
+(defn get-antinodes2 [[[row1 col1] [row2 col2]] matrix]
+  (let [[vec_row vec_col] [(- row2 row1) (- col2 col1)]
+        forward-points (get-in-direction [row2 col2] [vec_row vec_col] matrix)
+        backward-points (get-in-direction [row1 col1] [(- vec_row) (- vec_col)] matrix)]
+    (concat backward-points forward-points)))
+
+(defn get-antinodes-for-target2 [[target coordinates] matrix]
+  (let [paired-coords (every-pair coordinates)
+        all-anti (apply concat (map #(get-antinodes2 %1 matrix) paired-coords))]
+    all-anti))
+
+(defn solve2 [[signal-to-locations matrix]]
+  (let [signal-to-antinodes (map #(get-antinodes-for-target2 %1 matrix) signal-to-locations)
+        all-antinodes (set (apply concat signal-to-antinodes))] 
+    (count all-antinodes)))
+
+(defn part2 
+  ([] (part2 "day08/input.txt"))
+  ([file-name]
+  (-> file-name
+      (utils/read-file-lines)
+      (parse-input)
+      (solve2))))
--- 2024/aoc-2024/test/aoc_2024/day08/day08_test.clj
+++ 2024/aoc-2024/test/aoc_2024/day08/day08_test.clj
@@ -9,3 +9,15 @@
 (deftest day08-part1-test
       (testing "Day 08 part1"
         (is (= 341 (part1)))))
+
+(deftest day08-part2-ex1-test
+      (testing "Day 08 ex1 part2"
+        (is (= 34 (part2 "day08/ex1.txt")))))
+
+(deftest day08-part2-ex2-test
+      (testing "Day 08 ex2 part2"
+        (is (= 9 (part2 "day08/ex2.txt")))))
+
+(deftest day08-part2-test
+      (testing "Day 08 part2"
+        (is (= 1134 (part2)))))
