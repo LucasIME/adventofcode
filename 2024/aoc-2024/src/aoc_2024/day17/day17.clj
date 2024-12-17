3dc49f210dce150680011d5f3d4ec60beae1e90e
Lucas Meireles <meireles31@msn.com>
2024-12-17 09:30:32 +0000

    Added solution to 2024 Q17 part1

--- 2024/aoc-2024/src/aoc_2024/day17/day17.clj
+++ 2024/aoc-2024/src/aoc_2024/day17/day17.clj
@@ -17,10 +17,85 @@
         [_ raw-p-list] (str/split raw-program #"Program: ")
         registers (parse-registers raw-registers)
         p-list (map #(Integer/parseInt %1) (str/split raw-p-list #","))]
-    [registers p-list]))
+    [registers (vec p-list)]))
 
-(defn solve [input]
-  input)
+(defn combo [registers param]
+  (cond
+    (and (>= param 0) (<= param 3)) param
+    (= param 4) (get registers "A")
+    (= param 5) (get registers "B")
+    (= param 6) (get registers "C")))
+
+(defn adv [registers param ip output]
+  (let [numerator (get registers "A")
+        denominator (long (Math/pow 2 (combo registers param)))
+        result (Math/floorDiv numerator denominator)
+        updated-registers (assoc registers "A" result)]
+    [updated-registers (+ ip 2) output]))
+
+(defn bxl [registers param ip output]
+  (let [xor (bit-xor (get registers "B") param)
+        updated-registers (assoc registers "B" xor)]
+    [updated-registers (+ ip 2) output]))
+
+(defn bst [registers param ip output]
+  (let [result (rem (combo registers param) 8)
+        updated-registers (assoc registers "B" result)]
+    [updated-registers (+ ip 2) output]))
+
+(defn jnz [registers param ip output]
+  (if (= (get registers "A") 0)
+    [registers (+ ip 2) output]
+    [registers param output]))
+
+(defn bxc [registers param ip output]
+  (let [xor (bit-xor (get registers "B") (get registers "C"))
+        updated-registers (assoc registers "B" xor)]
+    [updated-registers (+ ip 2) output]))
+
+(defn out [registers param ip output]
+  (let [val (rem (combo registers param) 8)]
+    [registers (+ ip 2) (conj output val)]))
+
+(defn bdv [registers param ip output]
+  (let [numerator (get registers "A")
+        denominator (long (Math/pow 2 (combo registers param)))
+        result (Math/floorDiv numerator denominator)
+        updated-registers (assoc registers "B" result)]
+    [updated-registers (+ ip 2) output]))
+
+(defn cdv [registers param ip output]
+  (let [numerator (get registers "A")
+        denominator (long (Math/pow 2 (combo registers param)))
+        result (Math/floorDiv numerator denominator)
+        updated-registers (assoc registers "C" result)]
+    [updated-registers (+ ip 2) output]))
+
+
+(defn process-op-code [registers op-code param ip output]
+  (cond 
+    (= op-code 0) (adv registers param ip output)
+    (= op-code 1) (bxl registers param ip output)
+    (= op-code 2) (bst registers param ip output)
+    (= op-code 3) (jnz registers param ip output)
+    (= op-code 4) (bxc registers param ip output)
+    (= op-code 5) (out registers param ip output)
+    (= op-code 6) (bdv registers param ip output)
+    (= op-code 7) (cdv registers param ip output)))
+
+(defn run [registers program ip output]
+  (loop [registers registers, program program, ip ip, output output]
+      (if (>= ip (count program))
+      [registers output]
+        (let [op (nth program ip) 
+              param (nth program (inc ip)) 
+              [new-registers new-ip new-out] (process-op-code registers op param ip output)] 
+          (recur new-registers program new-ip new-out)))))
+
+(defn solve [[registers program]]
+  (let [[new-state output] (run registers program 0 [])]
+    (str/join "," output)))
+
 
 (defn part1 
   ([] (part1 "day17/input.txt"))
--- 2024/aoc-2024/test/aoc_2024/day17/day17_test.clj
+++ 2024/aoc-2024/test/aoc_2024/day17/day17_test.clj
@@ -4,8 +4,29 @@
 
 (deftest day17-part1-ex1-test
       (testing "Day 17 ex1"
-        (is (= "4,6,3,5,6,3,5,2,1,0" (part1 "day16/ex1.txt")))))
+        (is (= "4,6,3,5,6,3,5,2,1,0" (part1 "day17/ex1.txt")))))
+
+(deftest basic-ex1 
+  (testing "ex1"
+    (is (= [{"C" 9 "B" 1} []] (run {"C" 9} [2 6] 0 [])))))
+
+(deftest basic-ex2
+  (testing "ex2"
+    (is (= [{"A" 10} [0 1 2]] (run {"A" 10} [5 0 5 1 5 4] 0 [])))))
+
+
+(deftest basic-ex3
+  (testing "ex3"
+    (is (= [{"A" 0} [4 2 5 6 7 7 7 7 3 1 0]] (run {"A" 2024} [0 1 5 4 3 0] 0 [])))))
+
+(deftest basic-ex4
+  (testing "ex4"
+    (is (= [{"B" 26} []] (run {"B" 29} [1 7] 0 [])))))
+
+(deftest basic-ex5
+  (testing "ex5"
+    (is (= [{"B" 44354 "C" 43690} []] (run {"B" 2024 "C" 43690} [4 0] 0 [])))))
 
 (deftest day17-part1-test
       (testing "Day 17 part1"
-        (is (= -1 (part1)))))
+        (is (= "7,5,4,3,4,5,3,4,6" (part1)))))
