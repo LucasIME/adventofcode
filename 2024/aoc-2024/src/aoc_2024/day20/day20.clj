a394c9b8e49c927e426b45f662febf84a45b2d82
Lucas Meireles <meireles31@msn.com>
2024-12-20 10:44:45 +0000

    Added solution to 2024 Q20 part1

--- 2024/aoc-2024/src/aoc_2024/day20/day20.clj
+++ 2024/aoc-2024/src/aoc_2024/day20/day20.clj
@@ -1,6 +1,5 @@
 (ns aoc-2024.day20.day20
-  (:require [aoc-2024.utils.utils :as utils]
-            [clojure.set :as set]))
+  (:require [aoc-2024.utils.utils :as utils]))
 
 (def directions [[-1 0] [0 1] [1 0] [0 -1]])
 
@@ -25,35 +24,69 @@
     valid-neighs))
 
 (defn shortest-path [grid start end]
-  (loop [queue (conj (clojure.lang.PersistentQueue/EMPTY) [start 0])
-         visited #{}]
-    (let [[cur dist] (peek queue)]
+  (loop [queue (conj (clojure.lang.PersistentQueue/EMPTY) [start 0 nil])
+         visited #{}
+         prev-map {}]
+    (let [[cur dist before] (peek queue)]
       (cond
-        (= cur end) dist
+        (= cur end) [dist (assoc prev-map cur before)]
         (empty? queue) Integer/MAX_VALUE
-        (contains? visited cur) (recur (pop queue) visited)
+        (contains? visited cur) (recur (pop queue) visited prev-map)
         :else (let [new-visited (conj visited cur)
                     neighs (get-direct-neighs grid cur)
-                    new-q (reduce (fn [acc neigh] (conj acc [neigh (inc dist)])) (pop queue) neighs)]
-                (recur new-q new-visited))))))
+                    new-q (reduce (fn [acc neigh] (conj acc [neigh (inc dist) cur])) (pop queue) neighs)
+                    new-prev-map (if (not= nil before) (assoc prev-map cur before) prev-map)]
+                (recur new-q new-visited new-prev-map))))))
 
-(defn generate-all-cheat-grids [grid]
-  (for [row (range (count grid))
-        col (range (count (get grid row)))
-        :when (= (get-in grid [row col]) "#")]
-    (let [new-grid (assoc-in grid [row col] ".")]
-      new-grid)))
+(defn get-path [prev-map target-pos]
+  (loop [cur target-pos
+         path []]
+    (if (nil? cur)
+      path
+      (recur (get prev-map cur) (conj path cur)))))
+
+(defn pos-to-dist [path]
+  (let [pos-and-dist-pairs (map-indexed (fn [idx pos] [pos idx]) path)]
+    (into {} pos-and-dist-pairs)))
+
+(defn get-two-next-in-dir [pos dir]
+  (let [p1 (add-pos pos dir)
+        p2 (add-pos p1 dir)]
+    [p1 p2]))
+
+(defn get-cheat-neighs [grid pos known-dists]
+  (let [two-next-vec (map #(get-two-next-in-dir pos %1) directions)
+        cheat-neighs (filter (fn [[p1 p2]]
+                               (and
+                                (= (get-in grid p1) "#")
+                                (not= (get-in grid p2) "#")
+                                (contains? known-dists p2)))
+                             two-next-vec)]
+    (map #(second %1) cheat-neighs)))
+
+(defn collect-cheats [grid best-path known-dists]
+  (loop [pending best-path, cheats []]
+    (cond
+      (empty? pending) cheats
+      :else (let [cur (first pending)
+                  best-path-size (count best-path)
+                  dist-to-source (- best-path-size (known-dists cur) 1)
+                  neighs (get-cheat-neighs grid cur known-dists)
+                  new-cheats (map (fn [cheat-neigh]
+                                    (let [new-dist (+ dist-to-source 2 (get known-dists cheat-neigh))]
+                                      (- best-path-size new-dist 1))) ;; Not sure why I need the -1 here
+                                  neighs)
+                  good-cheats (filter #(> %1 0) new-cheats)
+                  new-cheat-out (reduce (fn [acc new] (conj acc new)) cheats good-cheats)]
+              (recur (rest pending) new-cheat-out)))))
 
 (defn solve [[grid start target-pos]]
-  (let [shortest (shortest-path grid start target-pos)
-        all-cheat-grids (generate-all-cheat-grids grid)
-        all-shorted-paths (map #(shortest-path %1 start target-pos) all-cheat-grids)
-        good-cheats (filter #(<= %1 (- shortest 100)) all-shorted-paths)]
-    ;; (println start target-pos)
-    ;; (println shortest)
-    ;; (println all-shorted-paths (frequencies (map #(- shortest %1) all-shorted-paths)))
-    (println (count good-cheats))
-    (count good-cheats)))
+  (let [[shortest prev-map] (shortest-path grid start target-pos)
+        best-path (get-path prev-map target-pos)
+        position-to-target-dist (pos-to-dist best-path)
+        all-cheats (collect-cheats grid best-path position-to-target-dist)
+        better-than-100-cheats (filter #(>= %1 100) all-cheats)]
+    (count better-than-100-cheats)))
 
 (defn part1
   ([] (part1 "day20/input.txt"))
@@ -62,5 +95,3 @@
        (utils/read-file-lines)
        (parse-input)
        (solve))))
-
-(part1 "day20/ex1.txt")
--- 2024/aoc-2024/test/aoc_2024/day20/day20_test.clj
+++ 2024/aoc-2024/test/aoc_2024/day20/day20_test.clj
@@ -1,11 +1,31 @@
 (ns aoc-2024.day20.day20-test
   (:require [clojure.test :refer :all]
-            [aoc-2024.day16.day16 :refer :all]))
+            [aoc-2024.day20.day20 :refer :all]
+            [aoc-2024.utils.utils :as utils]))
+
+(defn get-cheat-freq [file-name]
+  (let [[grid start target-pos] (-> file-name
+                                    (utils/read-file-lines)
+                                    (parse-input))
+        [shortest prev-map] (shortest-path grid start target-pos)
+        best-path (get-path prev-map target-pos)
+        position-to-target-dist (pos-to-dist best-path)]
+    (frequencies (collect-cheats grid best-path position-to-target-dist))))
 
 (deftest day20-part1-ex1-test
   (testing "Day 20 ex1"
-    (is (= -1 (part1 "day20/ex1.txt")))))
+    (is (= {2 14
+            4 14
+            6 2
+            8 4
+            10 2
+            12 3
+            20 1
+            36 1
+            38 1
+            40 1
+            64 1} (get-cheat-freq "day20/ex1.txt")))))
 
 (deftest day20-part1-test
   (testing "Day 20 part1"
-    (is (= -1 (part1)))))
+    (is (= 1404 (part1)))))
