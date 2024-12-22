3a309011f2f9e7d2e9a75163eee4ce8f95f55b7e
Lucas Meireles <meireles31@msn.com>
2024-12-22 09:34:13 +0000

    Added solution to 2024 Q22 part2

--- 2024/aoc-2024/src/aoc_2024/day22/day22.clj
+++ 2024/aoc-2024/src/aoc_2024/day22/day22.clj
@@ -41,3 +41,51 @@
        (utils/read-file-lines)
        (parse-input)
        (solve 2000))))
+
+(defn generate-secrets [start times]
+  (loop [cur start, n times, out [start]]
+    (cond
+      (= 1 n) out
+      :else (let [new-secret (apply-secret cur 1)]
+              (recur new-secret (dec n) (conj out new-secret))))))
+
+(defn differences [coll]
+  (let [paired (partition 2 1 coll)]
+    (map (fn [[a b]] (- b a)) paired)))
+
+(defn all-seq-and-prices [prices price-changes]
+  (let [four-diff-seqs (partition 4 1 price-changes)
+        four-diff-and-index (map-indexed (fn [i four-seq] [(vec four-seq) (+ i 4)]) four-diff-seqs)
+        seq-to-first-price (reduce (fn [acc [four-seq price-idx]]
+                                     (if (contains? acc four-seq) acc
+                                         (assoc acc four-seq (nth prices price-idx))))
+                                   {} four-diff-and-index)]
+    seq-to-first-price))
+
+(defn find-best-seq [best-seq-and-prices]
+  (let [all-seqs (reduce (fn [acc k] (conj acc k)) #{} (mapcat keys best-seq-and-prices))
+        [best-seq best-sum] (reduce (fn [[best-seq max-so-far] seq]
+                                      (let [new-sum (reduce + (map #(get % seq 0) best-seq-and-prices))]
+                                        (if (> new-sum max-so-far) [seq new-sum] [best-seq max-so-far])))
+                                    [nil Long/MIN_VALUE] all-seqs)]
+    best-seq))
+
+(defn solve2 [initial-secrets]
+  (let [sequences-of-secrets (map #(generate-secrets %1 2001) initial-secrets)
+        sequences-of-prices (vec (map (fn [secrets] (vec (map #(mod % 10) secrets)))
+                                      sequences-of-secrets))
+        sequences-of-price-changes (vec (map #(vec (differences %1)) sequences-of-prices))
+        best-seq-and-prices (for [i (range (count sequences-of-prices))]
+                              (all-seq-and-prices (nth sequences-of-prices i)
+                                                  (nth sequences-of-price-changes i)))
+        best-seq (find-best-seq best-seq-and-prices)
+        total (reduce + (map #(get % best-seq 0) best-seq-and-prices))]
+    total))
+
+(defn part2
+  ([] (part2 "day22/input.txt"))
+  ([file-name]
+   (-> file-name
+       (utils/read-file-lines)
+       (parse-input)
+       (solve2))))
--- 2024/aoc-2024/test/aoc_2024/day22/day22_test.clj
+++ 2024/aoc-2024/test/aoc_2024/day22/day22_test.clj
@@ -6,6 +6,14 @@
   (testing "Day 22 ex1"
     (is (= 37327623 (part1 "day22/ex1.txt")))))
 
-(deftest day22-part1-test
+(deftest ^:slow day22-part1-test
   (testing "Day 22 part1"
     (is (= 14392541715 (part1)))))
+
+(deftest day22-part2-ex2-test
+  (testing "Day 22 ex2 part 2"
+    (is (= 23 (part2 "day22/ex2.txt")))))
+
+(deftest ^:slow day22-part2-test
+  (testing "Day 22 part2"
+    (is (= 1628 (part2)))))
