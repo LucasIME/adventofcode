d513442dd5a9093eafdc52450a00d4f6e3e9603d
Lucas Meireles <meireles31@msn.com>
2024-12-25 21:03:44 +0100

    Added code used to solve 2024 Q24 part 2. It was a mix of graph inspection and some code to find wrongly set bits

--- 2024/aoc-2024/src/aoc_2024/day24/day24.clj
+++ 2024/aoc-2024/src/aoc_2024/day24/day24.clj
@@ -61,3 +61,68 @@
        (utils/read-file)
        (parse-input)
        (solve))))
+
+(defn print-sub-graph [sub-graph]
+  (println "subgraph {")
+  (print "rank = same;")
+  (run! (fn [[key val]] (print " " key ";")) sub-graph)
+  (println "}"))
+
+(defn print-graph [graph]
+  (println "digraph {")
+  (let [z-nodes (filter (fn [[key val]] (str/starts-with? key "z")) graph)
+        y-nodes (filter (fn [[key val]] (str/starts-with? key "y")) graph)
+        x-nodes (filter (fn [[key val]] (str/starts-with? key "x")) graph)
+        ]
+        (print-sub-graph (sort z-nodes))
+        (print-sub-graph (sort y-nodes))
+        (print-sub-graph (sort x-nodes)))
+  (run! (fn [[key val]]
+          (match/match [val]
+            [{:val val2}] (+ 1 1)
+            ;; [{:val val2}] (println val2 " -> " key ";")
+            [{:symbol1 symbol1 :op op :symbol2 symbol2}] (do 
+                                                           (let [op-name (str symbol1 op symbol2)]
+                                                           (println symbol1 " -> " op-name ";")
+                                                           (println symbol2 " -> " op-name ";")
+                                                           (println op-name " -> " key ";")))
+
+            )) graph)
+  (println "}"))
+
+(defn set-x-and-y [graph i]
+  (reduce (fn [acc index]
+            (let [acc-with-x (assoc acc (format "x%02d" index) {:val (if (= i index) 1 0)} )
+                  acc-with-y (assoc acc-with-x (format "y%02d" index) {:val (if (= i index) 1 0)} )]
+              acc-with-y
+              )) graph (range 46)))
+
+
+(defn swap [graph a b]
+  (let [cur-a (get graph a)
+        cur-b (get graph b)]
+    (assoc (assoc graph b cur-a) a cur-b)))
+
+(defn solve2 [graph]
+  (doseq [k (range 46)]
+    (let [shift k
+        new-graph (set-x-and-y graph shift)
+        new-graph2 (swap new-graph "z08" "cdj")
+        new-graph3 (swap new-graph2 "z16" "mrb")
+        new-graph4 (swap new-graph3 "z32" "gfm")
+        new-graph5 (swap new-graph4 "qjd" "dhm")
+        x (bit-shift-left 1 shift)
+        y (bit-shift-left 1 shift)
+        expected (+ x y)
+        actual (solve new-graph5)]
+      (if (not= actual expected)
+        (println "shift: " shift "x: " x "y: " y "expected: " expected "actual: " actual)
+        (+ 1 1)))))
+
+(defn part2
+  ([] (part2 "day24/input.txt"))
+  ([file-name]
+   (-> file-name
+       (utils/read-file)
+       (parse-input)
+       (solve2))))
