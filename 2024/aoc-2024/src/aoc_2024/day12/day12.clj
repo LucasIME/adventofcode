00a0f891206be9cfd8713b6fcdc8625976d23d48
Lucas Meireles <meireles31@msn.com>
2024-12-31 10:38:59 +0000

    Added solution to 2024 Q12 part2

--- 2024/aoc-2024/src/aoc_2024/day12/day12.clj
+++ 2024/aoc-2024/src/aoc_2024/day12/day12.clj
@@ -6,6 +6,11 @@
   (utils/to-char-matrix lines))
 
 (def directions [[1 0] [-1 0] [0 1] [0 -1]])
+(def directions-diagonal [[-1 -1] [-1 1] [1 1] [1 -1]])
+
+(defn get-neighs-on-dir [[row col] vectors]
+  (map (fn [[dr dc]] [(+ row dr) (+ col dc)]) vectors))
+
 
 (defn get-neigh-pos [row col]
   (map (fn [[dr dc]] [(+ row dr) (+ col dc)]) directions))
@@ -15,27 +20,27 @@
                 (= (get-in grid %1) (get-in grid [row col])))
           (get-neigh-pos row col)))
 
-(defn explore 
+(defn explore
   ([grid [row col]] (explore grid (list [row col]) #{}))
   ([grid pending visited]
-   (cond 
+   (cond
      (empty? pending) visited
      (contains? visited (first pending)) (explore grid (rest pending) visited)
-     :else (let [[row col] (first pending) 
+     :else (let [[row col] (first pending)
                  valid-neighs (get-valid-neighs grid row col)
                  new-pending (concat valid-neighs (rest pending))
-                 visited-with-me (conj visited [row col])] 
-             (explore grid 
+                 visited-with-me (conj visited [row col])]
+             (explore grid
                       new-pending
                       visited-with-me)))))
 
 (defn perimiter-pos [grid [row col]]
   (let [cur-val (get-in grid [row col])]
     (count  (filter #(not= cur-val (get-in grid %1))
-          (get-neigh-pos row col)))))
+                    (get-neigh-pos row col)))))
 
-(defn perimeter [grid island] 
-  (let [result (reduce + (map #(perimiter-pos grid %1) island))] 
+(defn perimeter [grid island]
+  (let [result (reduce + (map #(perimiter-pos grid %1) island))]
     result))
 
 (defn island-score [grid island]
@@ -47,31 +52,61 @@
                         col (range (count (get grid row)))]
                     [row col])]
     (loop [to-visit positions, all-visited #{}, score 0]
-      (cond 
+      (cond
         (empty? to-visit) score
         (contains? all-visited (first to-visit)) (recur (rest to-visit) all-visited score)
         :else (let [island (explore grid (first to-visit))]
                 (recur (rest to-visit) (set/union all-visited (set island)) (+ score (scoring-fn grid island))))))))
 
-(defn part1 
+(defn part1
   ([] (part1 "day12/input.txt"))
   ([file-name]
-  (-> file-name
-      (utils/read-file-lines)
-      (parse-input)
-      (solve island-score))))
+   (-> file-name
+       (utils/read-file-lines)
+       (parse-input)
+       (solve island-score))))
 
-(defn sides [grid island]
-  0)
+(defn expand [[row col]]
+  (let [double-row (* 2 row)
+        double-col (* 2 col)]
+    [[double-row double-col]
+     [(inc double-row) double-col]
+     [(+ double-row 2) double-col]
+     [double-row (inc double-col)]
+     [double-row (+ double-col 2)]
+     [(inc double-row) (inc double-col)]
+     [(inc double-row) (+ double-col 2)]
+     [(+ double-row 2) (inc double-col)]
+     [(+ double-row 2) (+ double-col 2)]]))
+
+(defn sides [island]
+  (let [expanded-island-set (set (mapcat expand island))
+        vertice-count (reduce (fn [acc point]
+                                (let [all-neigh (filter #(contains? expanded-island-set %)
+                                                        (get-neighs-on-dir point (concat directions directions-diagonal)))
+                                      diagonal-neigh (filter #(contains? expanded-island-set %)
+                                                             (get-neighs-on-dir point directions-diagonal))]
+                                  (cond
+                                    (or
+                                     (= 3 (count all-neigh))
+                                     (and
+                                      (= 7 (count all-neigh))
+                                      (= 3 (count diagonal-neigh)))) (inc acc)
+                                    (and
+                                     (= 6 (count all-neigh))
+                                     (= 2 (count diagonal-neigh))) (+ acc 2)
+                                    :else acc)))
+                              0 expanded-island-set)]
+    vertice-count))
 
 (defn island-score2 [grid island]
   (* (count island)
-     (sides grid island)))
+     (sides island)))
 
-(defn part2 
+(defn part2
   ([] (part2 "day12/input.txt"))
   ([file-name]
-  (-> file-name
-      (utils/read-file-lines)
-      (parse-input)
-      (solve island-score2))))
+   (-> file-name
+       (utils/read-file-lines)
+       (parse-input)
+       (solve island-score2))))
--- 2024/aoc-2024/test/aoc_2024/day12/day12_test.clj
+++ 2024/aoc-2024/test/aoc_2024/day12/day12_test.clj
@@ -3,17 +3,37 @@
             [aoc-2024.day12.day12 :refer :all]))
 
 (deftest day12-part1-ex1-test
-      (testing "Day 12 ex1"
-        (is (= 140 (part1 "day12/ex1.txt")))))
+  (testing "Day 12 ex1"
+    (is (= 140 (part1 "day12/ex1.txt")))))
 
 (deftest day12-part1-ex2-test
-      (testing "Day 12 ex2"
-        (is (= 772 (part1 "day12/ex2.txt")))))
+  (testing "Day 12 ex2"
+    (is (= 772 (part1 "day12/ex2.txt")))))
 
 (deftest day12-part1-ex3-test
-      (testing "Day 12 ex3"
-        (is (= 1930 (part1 "day12/ex3.txt")))))
+  (testing "Day 12 ex3"
+    (is (= 1930 (part1 "day12/ex3.txt")))))
 
 (deftest day12-part1-test
-      (testing "Day 12 part1"
-        (is (= 1344578 (part1)))))
+  (testing "Day 12 part1"
+    (is (= 1344578 (part1)))))
+
+(deftest day12-part2-ex1-test
+  (testing "Day 12 ex1 part 2"
+    (is (= 80 (part2 "day12/ex1.txt")))))
+
+(deftest day12-part2-ex2-test
+  (testing "Day 12 ex2 part 2"
+    (is (= 436 (part2 "day12/ex2.txt")))))
+
+(deftest day12-part2-ex4-test
+  (testing "Day 12 ex4 part 2"
+    (is (= 236 (part2 "day12/ex4.txt")))))
+
+(deftest day12-part2-ex5-test
+  (testing "Day 12 ex5 part 2"
+    (is (= 368 (part2 "day12/ex5.txt")))))
+
+(deftest day12-part2-test
+  (testing "Day 12 part2"
+    (is (= 814302 (part2)))))
