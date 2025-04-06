c62eb2daebadddcad9598db1df4d59ff2bdf6c9e
Lucas Meireles <meireles31@msn.com>
2025-04-06 17:40:37 +0100

    Refactoring day 01

---
+++ 2019/tests/day01_test.rs
@@ -0,0 +1,14 @@
+#[cfg(test)]
+mod tests {
+    use aoc2019::day01;
+
+    #[test]
+    fn part1_works() {
+        assert_eq!(3339288, day01::part1());
+    }
+
+    #[test]
+    fn part2_works() {
+        assert_eq!(5006064, day01::part2());
+    }
+}
