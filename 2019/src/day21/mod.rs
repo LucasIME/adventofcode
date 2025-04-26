fabf941d3fc81ef19f0bae4f1258a869c2086517
Lucas Meireles <meireles31@msn.com>
2025-04-26 12:41:09 +0100

    Added solution to 2019 Q21 part 2

--- 2019/src/day21/mod.rs
+++ 2019/src/day21/mod.rs
@@ -46,3 +46,43 @@ pub fn part1() -> isize {
     print_out(&mut computer);
     return computer.get_last_out();
 }
+
+pub fn part2() -> isize {
+    let input = std::fs::read_to_string("resources/day21/day21.txt")
+        .unwrap()
+        .trim()
+        .to_string();
+    let op_map = parse_opcodes(input);
+
+    let instructions = vec![
+        "NOT B J",
+        "NOT C T",
+        "OR T J",
+        "AND D J",
+        "AND H J",
+        "NOT A T",
+        "OR T J",
+        "RUN",
+        "",];
+    let instructions_ascii : VecDeque<isize> = instructions
+        .join("\n")
+        .chars()
+        .map(|c| c as isize)
+        .collect();
+
+    let mut computer = Computer {
+        input: instructions_ascii,
+        output: vec![],
+        memory: op_map,
+        cur_pos: 0,
+        last_intruction: 0,
+        relative_base_offset: 0,
+    };
+
+    while computer.last_intruction != 99 {
+        computer.process_until_break_or_out();
+    }
+
+    print_out(&mut computer);
+    return computer.get_last_out();
+}
--- 2019/tests/day21_test.rs
+++ 2019/tests/day21_test.rs
@@ -6,4 +6,9 @@ mod tests {
     fn part1_works() {
         assert_eq!(19361332, day21::part1());
     }
+
+    #[test]
+    fn part2_works() {
+        assert_eq!(1143351187, day21::part2());
+    }
 }
