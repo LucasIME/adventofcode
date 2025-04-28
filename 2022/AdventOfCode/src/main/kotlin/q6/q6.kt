f108abe2c71806a8bd53491384efe359e6a23325
Lucas Meireles <meireles31@msn.com>
2025-04-28 16:54:39 +0100

    update q6

--- 2022/AdventOfCode/src/main/kotlin/q6/q6.kt
+++ 2022/AdventOfCode/src/main/kotlin/q6/q6.kt
@@ -1,13 +1,9 @@
 package q6
 
-import java.io.File
-
-fun main() {
-    val input = parseInput()
-    println(process(input))
-}
+import java.nio.file.Path
 
 private const val EMPTY = '$'
+private const val WINDOW_SIZE = 14
 
 private fun process(input: String): Int {
     val window = CharArray(4) { EMPTY }
@@ -36,6 +32,31 @@ private fun areAllDifferent(window: CharArray): Boolean {
     return EMPTY !in set
 }
 
-private fun parseInput(): String {
-    return File("src/main/resources/q6.txt").readLines().first()
+private fun parseInput(inputPath: Path): String {
+    return inputPath.toFile().readLines().first()
+}
+
+fun part1(inputPath: Path): Int {
+    val input = parseInput(inputPath)
+    return process(input)
+}
+
+private fun process2(input: String): Int {
+    val window = CharArray(WINDOW_SIZE) { EMPTY }
+    var windowEndIndex = -1
+    for (i in input.indices) {
+        val c = input[i]
+        window[i % WINDOW_SIZE] = c
+        if (areAllDifferent(window)) {
+            windowEndIndex = i
+            break
+        }
+    }
+
+    return windowEndIndex + 1
+}
+
+fun part2(inputPath: Path): Int {
+    val input = parseInput(inputPath)
+    return process2(input)
 }
