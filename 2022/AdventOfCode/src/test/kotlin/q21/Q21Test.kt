676f8a50517b35cb2b19693986b7b3419a54c32d
Lucas Meireles <meireles31@msn.com>
2025-04-29 13:21:52 +0100

    update q21

---
+++ 2022/AdventOfCode/src/test/kotlin/q21/Q21Test.kt
@@ -0,0 +1,13 @@
+package q21
+
+import junit.framework.TestCase.assertEquals
+import java.nio.file.Paths
+import kotlin.test.Test
+
+class Q21Test {
+    @Test
+    fun `part 1 works`()  {
+        val filePath = javaClass.getResource("/q21.txt")?.toURI()?.let { Paths.get(it) }
+        assertEquals(93813115694560, part1(filePath!!) )
+    }
+}
