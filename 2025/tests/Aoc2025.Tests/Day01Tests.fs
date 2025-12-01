c6842df12d6bdbabfe9e69e10a5d887c3070470b
Lucas Meireles <meireles31@msn.com>
2025-12-01 10:37:50 +0000

    Added solution to 2025 Q1 part1

--- 2025/tests/Aoc2025.Tests/Day01Tests.fs
+++ 2025/tests/Aoc2025.Tests/Day01Tests.fs
@@ -7,6 +7,12 @@ module Day01Tests =
 
     [<Fact>]
     let ``Part 1 Example Test`` () =
-        let input = System.IO.File.ReadAllText("resources/day01.txt")
+        let input = System.IO.File.ReadAllText("resources/day01/ex1.txt")
         let result = Day01.part1 input
-        Assert.Equal(4, result)
+        Assert.Equal(3, result)
+
+    [<Fact>]
+    let ``Part 1 Test`` () =
+        let input = System.IO.File.ReadAllText("resources/day01/input.txt")
+        let result = Day01.part1 input
+        Assert.Equal(1165, result)
