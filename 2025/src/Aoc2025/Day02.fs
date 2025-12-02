4e9005c683fe8f61cfc17e9da86a5c8947501ed5
Lucas Meireles <meireles31@msn.com>
2025-12-02 08:37:44 +0000

    Added solution to 2025 Q2 part 2

--- 2025/src/Aoc2025/Day02.fs
+++ 2025/src/Aoc2025/Day02.fs
@@ -21,3 +21,22 @@ module Day02 =
         let intervals = lines |> Array.map parseEntry
         let expandedIntervals = intervals |> Array.collect (fun (s, e) -> [|s .. e|])
         expandedIntervals |> Array.filter isInvalid |> Array.sum
+
+    let allEqual lst =
+        match lst with
+        | [] -> true
+        | x :: xs -> Seq.forall ((=) x) xs
+
+    let isInvalid2(value: int64) =
+        let str = value.ToString()
+        let length = str.Length
+
+        [1..length/2] 
+            |> List.map (fun k ->  str |> Seq.chunkBySize k) 
+            |> List.exists (fun chunkList -> allEqual   (Seq.toList chunkList))
+
+    let part2 (input: string) =
+        let lines = input.Split(',')
+        let intervals = lines |> Array.map parseEntry
+        let expandedIntervals = intervals |> Array.collect (fun (s, e) -> [|s .. e|])
+        expandedIntervals |> Array.filter isInvalid2 |> Array.sum
--- 2025/tests/Aoc2025.Tests/Day02Tests.fs
+++ 2025/tests/Aoc2025.Tests/Day02Tests.fs
@@ -16,3 +16,15 @@ module Day02Tests =
         let input = System.IO.File.ReadAllText("resources/day02/input.txt")
         let result = Day02.part1 input
         Assert.Equal(22062284697L, result)
+
+    [<Fact>]
+    let ``Part 2 Example Test`` () =
+        let input = System.IO.File.ReadAllText("resources/day02/ex1.txt")
+        let result = Day02.part2 input
+        Assert.Equal(4174379265L, result)
+
+    [<Fact>]
+    let ``Part 2 Test`` () =
+        let input = System.IO.File.ReadAllText("resources/day02/input.txt")
+        let result = Day02.part2 input
+        Assert.Equal(46666175279L, result)
