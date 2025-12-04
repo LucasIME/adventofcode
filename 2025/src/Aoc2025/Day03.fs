2711937a20322593140cd8e3d6a82955818f45a6
Lucas Meireles <meireles31@msn.com>
2025-12-04 13:30:37 +0000

    Added solution to 2025 Q3 part2

--- 2025/src/Aoc2025/Day03.fs
+++ 2025/src/Aoc2025/Day03.fs
@@ -1,23 +1,34 @@
 namespace Aoc2025
 
 module Day03 =
+    let rec joltage (digits: int) (bank: array<int64>) : int64 = 
+        if digits = 1 then
+            bank |> Array.max |> int64
+        else
+            let prefix = bank |> Array.take (bank.Length - digits + 1)
+            let (rightmostBiggestDigit, rightmostBiggestDigitIndex) = 
+                prefix |> Array.mapi (fun i v -> v, i) |> Array.maxBy fst
+            let baseNum = rightmostBiggestDigit * (pown 10L (digits - 1)) 
+            baseNum + (joltage (digits - 1) bank[rightmostBiggestDigitIndex+1..])
 
-    let rec joltageHelper(digits: list<int>, maxDigitSoFar: int, maxTotalSoFar: int) : int  =
-        match digits with
-            | [] -> maxTotalSoFar
-            | head :: tail -> 
-                let newMaxDigit = max head maxDigitSoFar
-                let maxWithCurDigit = 10*maxDigitSoFar + head 
-                let newMaxTotal = max maxTotalSoFar maxWithCurDigit
-                joltageHelper(tail, newMaxDigit, newMaxTotal)
-
-    let joltage(bank: string) : int =
-        let digits =  bank |> Seq.map (fun c -> int c - int '0') |> Seq.toList
-        joltageHelper(digits, 0, 0)
-
-    let part1 (input: string) =
+    let solve (input: string) n =
         let lines = input.Split('\n')
         
-        let joltages = lines |> Array.map joltage
+        let lineAsDigits = 
+            lines 
+                |> Array.map (fun line -> 
+                    line 
+                        |> Seq.map (fun c -> int64 c - int64 '0') |> Seq.toArray)
+        let joltages = 
+            lineAsDigits 
+            |> Array.map (fun line -> joltage n line)
 
         joltages |> Array.sum
+
+
+    let part1 (input: string) =
+        solve input 2
+
+
+    let part2 (input: string) =
+        solve input 12
--- 2025/tests/Aoc2025.Tests/Day03Tests.fs
+++ 2025/tests/Aoc2025.Tests/Day03Tests.fs
@@ -9,10 +9,22 @@ module Day03Tests =
     let ``Part 1 Example Test`` () =
         let input = System.IO.File.ReadAllText("resources/day03/ex1.txt")
         let result = Day03.part1 input
-        Assert.Equal(357, result)
+        Assert.Equal(357L, result)
 
     [<Fact>]
     let ``Part 1 Test`` () =
         let input = System.IO.File.ReadAllText("resources/day03/input.txt")
         let result = Day03.part1 input
-        Assert.Equal(17229, result)
+        Assert.Equal(17229L, result)
+
+    [<Fact>]
+    let ``Part 2 Example Test`` () =
+        let input = System.IO.File.ReadAllText("resources/day03/ex1.txt")
+        let result = Day03.part2 input
+        Assert.Equal(3121910778619L, result)
+
+    [<Fact>]
+    let ``Part 2 Test`` () =
+        let input = System.IO.File.ReadAllText("resources/day03/input.txt")
+        let result = Day03.part2 input
+        Assert.Equal(170520923035051L, result)
