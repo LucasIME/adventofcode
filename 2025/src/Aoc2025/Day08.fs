307a158a8ad47a7a3c1ddafac12152f5e182e6b6
Lucas Sousa Meireles <meireles31@msn.com>
2025-12-08 10:32:20 +0000

    Added solution to 2025 Q8 part1

---
+++ 2025/src/Aoc2025/Day08.fs
@@ -0,0 +1,82 @@
+namespace Aoc2025
+
+module Day08 =
+
+    type Pos = int64 * int64 * int64
+
+    type UnionFind<'T when 'T : equality>() = 
+        let parent = System.Collections.Generic.Dictionary<'T, 'T>()
+        let size = System.Collections.Generic.Dictionary<'T, int>()
+
+        member this.Find(x: 'T) : 'T =
+            if not (parent.ContainsKey(x)) then
+                parent.[x] <- x
+                size.[x] <- 1
+
+            if parent.[x] <> x then
+                this.Find(parent.[x])
+            else
+                x
+
+        member this.Union(x: 'T, y: 'T) : unit =
+            let rootX = this.Find(x)
+            let rootY = this.Find(y)
+
+            let xHeight = size.[rootX]
+            let yHeight = size.[rootY]
+
+            if yHeight = xHeight then
+                parent[rootY] <- rootX
+                size[rootX] <- xHeight + 1
+            elif yHeight < xHeight then
+                parent[rootY] <- rootX
+            else
+                parent[rootX] <- rootY
+
+    let pairs list =
+        list
+        |> List.mapi (fun i x -> 
+            list
+            |> List.mapi (fun j y -> 
+                (i, j, x, y)
+            )
+            |> List.filter (fun (i, j, _, _) -> j > i)
+            |> List.map (fun (_, _, x, y) -> (x, y))
+        )
+        |> List.concat
+
+    let dist (x1, y1, z1) (x2, y2, z2) =
+        let dx = x1 - x2
+        let dy = y1 - y2
+        let dz = z1 - z2
+        sqrt (float (dx*dx + dy*dy + dz*dz))
+
+    let part1 (input: string) (n: int) =
+        let positions = 
+            input.Split('\n') |> Array.map (fun line -> 
+                let parts = line.Split(',')
+                (int64 parts.[0], int64 parts.[1], int64 parts.[2])
+        )
+
+        let posPairs = positions |> Array.toList |> pairs
+
+        let closestPairs = 
+            List.sortBy (fun (p1, p2) -> dist p1 p2) posPairs 
+
+        let firstNPairs = closestPairs |> List.take n
+
+        let unionFind = UnionFind<Pos>()
+
+        firstNPairs |> Seq.iter (fun (pos1, pos2) -> unionFind.Union(pos1, pos2))
+
+        let distinctPoints = firstNPairs |> List.collect (fun (p1, p2) -> [p1; p2])  |> List.distinct
+
+        let countBySet = distinctPoints |> List.countBy (fun p -> unionFind.Find(p))
+
+        let setByDecreasingCount = List.sortByDescending (fun (set, count ) -> count) countBySet
+
+        let threeLargest = setByDecreasingCount |> List.take 3
+
+        let threeCount = threeLargest |> List.map (fun (pos, count) -> count)
+
+        threeCount |> List.reduce (*)
