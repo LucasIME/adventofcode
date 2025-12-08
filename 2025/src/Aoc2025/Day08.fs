9e6de7b689959decaee272872c721b92c1ac0fda
Lucas Meireles <meireles31@msn.com>
2025-12-08 10:30:31 +0000

    solve part1

--- 2025/src/Aoc2025/Day08.fs
+++ 2025/src/Aoc2025/Day08.fs
@@ -1,26 +1,37 @@
 namespace Aoc2025
-open System
 
 module Day08 =
 
     type Pos = int64 * int64 * int64
 
-    type UnionFind<'T> = 
-        member this.parent = System.Collections.Generic.Dictionary<'T, 'T>
-        member this.size = System.Collections.Generic.Dictionary<'T, int>
+    type UnionFind<'T when 'T : equality>() = 
+        let parent = System.Collections.Generic.Dictionary<'T, 'T>()
+        let size = System.Collections.Generic.Dictionary<'T, int>()
 
         member this.Find(x: 'T) : 'T =
-            if not (this.parent.ContainsKey(x)) then
-                this.parent.[x] <- x
-            if this.parent.[x] <> x then
-                this.parent.[x] <- this.Find(this.parent.[x])
-            this.parent.[x]
+            if not (parent.ContainsKey(x)) then
+                parent.[x] <- x
+                size.[x] <- 1
+
+            if parent.[x] <> x then
+                this.Find(parent.[x])
+            else
+                x
 
         member this.Union(x: 'T, y: 'T) : unit =
             let rootX = this.Find(x)
             let rootY = this.Find(y)
-            if rootX <> rootY then
-                if this.size
+
+            let xHeight = size.[rootX]
+            let yHeight = size.[rootY]
+
+            if yHeight = xHeight then
+                parent[rootY] <- rootX
+                size[rootX] <- xHeight + 1
+            elif yHeight < xHeight then
+                parent[rootY] <- rootX
+            else
+                parent[rootX] <- rootY
 
     let pairs list =
         list
@@ -53,3 +64,28 @@ module Day08 =
             List.sortBy (fun (p1, p2) -> dist p1 p2) posPairs 
 
         let firstNPairs = closestPairs |> List.take n
+
+        let unionFind = UnionFind<Pos>()
+
+        firstNPairs |> Seq.iter (fun (pos1, pos2) -> unionFind.Union(pos1, pos2))
+
+        let distinctPoints = firstNPairs |> List.collect (fun (p1, p2) -> [p1; p2])  |> List.distinct
+
+        let countBySet = distinctPoints |> List.countBy (fun p -> unionFind.Find(p))
+
+        let setByDecreasingCount = List.sortByDescending (fun (set, count ) -> count) countBySet
+
+        let threeLargest = setByDecreasingCount |> List.take 3
+
+        let threeCount = threeLargest |> List.map (fun (pos, count) -> count)
+
+        threeCount |> List.reduce (*)
+
+
+
+
+
+
+
+        
+
--- 2025/tests/Aoc2025.Tests/Day08Tests.fs
+++ 2025/tests/Aoc2025.Tests/Day08Tests.fs
@@ -9,10 +9,10 @@ module Day08Tests =
     let ``Part 1 Example Test`` () =
         let input = System.IO.File.ReadAllText("resources/day08/ex1.txt")
         let result = Day08.part1 input 10
-        Assert.Equal(21, result)
+        Assert.Equal(40, result)
 
     [<Fact>]
     let ``Part 1 Test`` () =
         let input = System.IO.File.ReadAllText("resources/day08/input.txt")
         let result = Day08.part1 input 1000
-        Assert.Equal(1613, result)
+        Assert.Equal( 117000, result)
