925757ce413cd24efd13ef07a8d9d8328d91f098
Lucas Meireles <meireles31@msn.com>
2025-12-10 09:54:12 +0000

    trying to solve part2 with bfs

--- 2025/src/Aoc2025/Day10.fs
+++ 2025/src/Aoc2025/Day10.fs
@@ -96,14 +96,66 @@ module Day10 =
 
         resp
 
-    let costToTarget (machine: Machine) : int64 =
-        bfs machine
-
     let part1 (input: string) =
         let machines = parse input
 
         let distances =
             machines
-            |> List.map costToTarget
+            |> List.map bfs 
+
+        distances |> List.sum
+
+    let getStateAfterPress2 (state: int64 array, button: Buttons) : int64 array =
+        let newState = Array.copy state
+
+        button |> Array.iter (fun pos -> 
+            let newValue = state.[int pos] + 1L
+            newState.[int pos] <- newValue
+        )
+
+        newState
+
+    let canReachDest (state: int64 array) (desired: int64 array) : bool =
+        Array.forall2 (fun curStateVal desiredStateVal -> curStateVal <= desiredStateVal) state desired
+
+    let getNeighs2 (state: int64 array, buttons: Buttons array) : int64 array array =
+        buttons |> Array.map (fun button -> getStateAfterPress2(state, button))
+
+    let getValidNeighs2 (state: int64 array, buttons: Buttons array, desired: int64 array) : int64 array array =
+        let neighs = getNeighs2 (state, buttons)
+        neighs |> Array.filter (fun neighState -> canReachDest neighState desired)
+
+    let bfs2 (machine: Machine) : int64 =
+        let visited = HashSet<int64 array>()
+        let q = Queue<int64 array * int64>()
+
+        let startPos = machine.costs |> Array.map (fun n -> 0L)
+        let desiredPos = machine.costs
+
+        q.Enqueue(startPos, 0)
+
+        let mutable resp = -1L
+        let mutable finished: bool = false
+        while q.Count > 0 && (not finished) do
+            let pos, dist = q.Dequeue()
+            if pos = desiredPos then
+                resp <- dist
+                finished <- true
+            else
+                if not (visited.Contains(pos)) then
+                    visited.Add(pos) |> ignore
+                    let nextStates = getValidNeighs2(pos, machine.buttons, desiredPos)
+                    nextStates |> Array.iter (fun nextState -> q.Enqueue(nextState, dist + 1L))
+
+        resp
+
+    let part2 (input: string) =
+        let machines = parse input
+
+        let distances =
+            machines
+            |> List.map bfs2 
+
+        printfn "distances %A" distances
 
         distances |> List.sum
--- 2025/tests/Aoc2025.Tests/Day10Tests.fs
+++ 2025/tests/Aoc2025.Tests/Day10Tests.fs
@@ -15,4 +15,16 @@ module Day10Tests =
     let ``Part 1 Test`` () =
         let input = System.IO.File.ReadAllText("resources/day10/input.txt")
         let result = Day10.part1 input
-        Assert.Equal( 396L, result)
+        Assert.Equal(396L, result)
+
+    [<Fact>]
+    let ``Part 2 Example Test`` () =
+        let input = System.IO.File.ReadAllText("resources/day10/ex1.txt")
+        let result = Day10.part2 input
+        Assert.Equal(33L, result)
+
+    [<Fact>]
+    let ``Part 2 Test`` () =
+        let input = System.IO.File.ReadAllText("resources/day10/input.txt")
+        let result = Day10.part2 input
+        Assert.Equal(-1L, result)
